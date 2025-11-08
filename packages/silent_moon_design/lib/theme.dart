e840b9627d3e320b5e754372f62a6b006427cdfe
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-11-08 18:23:48 +0500

    feat(theme): introduce comprehensive Material TextTheme configuration

--- packages/silent_moon_design/lib/theme.dart
+++ packages/silent_moon_design/lib/theme.dart
@@ -21,9 +21,78 @@ class SilentMoonTheme {
       onTertiary: SilentMoonSemanticColor.tertiary.on,
     );
 
+    const textTheme = TextTheme(
+      displayLarge: TextStyle(
+        fontSize: SilentMoonFontSize.max,
+        fontWeight: SilentMoonFontWeight.extraBold,
+      ),
+      displayMedium: TextStyle(
+        fontSize: SilentMoonFontSize.loose,
+        fontWeight: SilentMoonFontWeight.bold,
+      ),
+      displaySmall: TextStyle(
+        fontSize: SilentMoonFontSize.wide,
+        fontWeight: SilentMoonFontWeight.semiBold,
+      ),
+
+      headlineLarge: TextStyle(
+        fontSize: SilentMoonFontSize.high,
+        fontWeight: SilentMoonFontWeight.semiBold,
+      ),
+      headlineMedium: TextStyle(
+        fontSize: SilentMoonFontSize.mid,
+        fontWeight: SilentMoonFontWeight.semiBold,
+      ),
+      headlineSmall: TextStyle(
+        fontSize: SilentMoonFontSize.base,
+        fontWeight: SilentMoonFontWeight.medium,
+      ),
+
+      titleLarge: TextStyle(
+        fontSize: SilentMoonFontSize.high,
+        fontWeight: SilentMoonFontWeight.medium,
+      ),
+      titleMedium: TextStyle(
+        fontSize: SilentMoonFontSize.mid,
+        fontWeight: SilentMoonFontWeight.medium,
+      ),
+      titleSmall: TextStyle(
+        fontSize: SilentMoonFontSize.low,
+        fontWeight: SilentMoonFontWeight.medium,
+      ),
+
+      bodyLarge: TextStyle(
+        fontSize: SilentMoonFontSize.mid,
+        fontWeight: SilentMoonFontWeight.normal,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: SilentMoonFontSize.base,
+        fontWeight: SilentMoonFontWeight.normal,
+      ),
+      bodySmall: TextStyle(
+        fontSize: SilentMoonFontSize.low,
+        fontWeight: SilentMoonFontWeight.normal,
+      ),
+
+      labelLarge: TextStyle(
+        fontSize: SilentMoonFontSize.base,
+        fontWeight: SilentMoonFontWeight.semiBold,
+      ),
+      labelMedium: TextStyle(
+        fontSize: SilentMoonFontSize.low,
+        fontWeight: SilentMoonFontWeight.semiBold,
+      ),
+      labelSmall: TextStyle(
+        fontSize: SilentMoonFontSize.tight,
+        fontWeight: SilentMoonFontWeight.medium,
+      ),
+    );
+
+    const fontFamily = SilentMoonFontFamily.helveticaNeue;
     return ThemeData(
       colorScheme: colorScheme,
-      fontFamily: SilentMoonFontFamily.helveticaNeue,
+      fontFamily: fontFamily,
+      textTheme: textTheme,
       extensions: [
         _buildTextFieldTheme(),
         _buildButtonTheme(colorScheme),
